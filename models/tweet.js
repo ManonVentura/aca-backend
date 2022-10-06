0ed703dceb6c397da7220eaac762324629c672ab
Nadia <nadiamahmoud.09@gmail.com>
2022-10-06 16:28:24 +0200

    lastcommit

--- models/tweet.js
+++ models/tweet.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const tweetSchema = mongoose.Schema({
   content: String,
   users : { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+  token:String,
   
 });
 
